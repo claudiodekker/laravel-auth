0ee33b43ec0027084a250488ae4454e5c4878e2c
Claudio Dekker <claudio@ubient.net>
2023-04-03 13:49:32 +0200

    Tests: Fix occasional account recovery regression

--- packages/core/src/Testing/Partials/RateLimiting/AccountRecoveryRequestRateLimitingTests.php
+++ packages/core/src/Testing/Partials/RateLimiting/AccountRecoveryRequestRateLimitingTests.php
@@ -3,6 +3,7 @@
 namespace ClaudioDekker\LaravelAuth\Testing\Partials\RateLimiting;
 
 use Illuminate\Auth\Events\Lockout;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Notification;
@@ -14,6 +15,7 @@ trait AccountRecoveryRequestRateLimitingTests
     /** @test */
     public function account_recovery_requests_are_rate_limited_after_too_many_requests(): void
     {
+        Carbon::setTestNow(now());
         Notification::fake();
         Event::fake([Lockout::class]);
         Collection::times(5)->each(fn () => RateLimiter::hit('account-recovery|127.0.0.1'));
@@ -26,6 +28,7 @@ trait AccountRecoveryRequestRateLimitingTests
         $this->assertSame(['email' => [__('laravel-auth::auth.recovery.throttle', ['seconds' => 60])]], $response->exception->errors());
         Event::assertDispatched(Lockout::class, fn (Lockout $event) => $event->request === request());
         Notification::assertNothingSent();
+        Carbon::setTestNow();
     }
 
     /** @test */
